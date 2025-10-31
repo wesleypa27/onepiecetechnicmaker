aa69ea1cb6f0164e77c2dd814752b1fc71ddbcdd
wesleypa27 <wesleypa27@hotmail.com>
2025-10-31 15:40:33 -0300

    att

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
-  <label> Gerador de tecnicas </label>
-  <button></button>
-  <button></button>
-  <Tabs>
-    <Tab name="Nivel">
-      <div class="flex flex-col gap-2 mt-2 w-32">
-        <button v-for="level in levels" :key="level_key(level)" @click="selected = level_key(level)"
-          class="px-4 py-2 rounded text-white transition" :class="selected === level_key(level)
-            ? 'bg-blue-600 font-bold'
-            : 'bg-gray-400 hover:bg-gray-500'">
-          {{ "Nível " + level }}
-        </button>
-      </div>
-    </Tab>
+  <div>
+    <label> Gerador de tecnicas </label>
+    <div>
+      <button key="technic_fight" @click="selected_type = 'fight'"
+        class="px-4 py-2 rounded text-white transition" :class="selected_type === 'fight'
+          ? 'bg-blue-600 font-bold'
+          : 'bg-gray-400 hover:bg-gray-500'">
+        Tecnica de luta
+      </button>
+      <button key="technic_fruit" @click="selected_type = 'fruit'"
+        class="px-4 py-2 rounded text-white transition" :class="selected_type === 'fruit'
+          ? 'bg-blue-600 font-bold'
+          : 'bg-gray-400 hover:bg-gray-500'">
+        Tecnica de akuma no mi
+      </button>
+    </div>
+    <Tabs>
+      <Tab name="Nivel">
+        <div class="flex flex-col gap-2 mt-2 w-32">
+          <button v-for="level in levels" :key="level_key(level)" @click="selected_level = level_key(level)"
+            class="px-4 py-2 rounded text-white transition" :class="selected_level === level_key(level)
+              ? 'bg-blue-600 font-bold'
+              : 'bg-gray-400 hover:bg-gray-500'">
+            {{ "Nível " + level }}
+          </button>
+        </div>
+      </Tab>
 
-    <Tab name="Tipo">
-      <p>text</p>
-    </Tab>
+      <Tab name="Tipo">
+        <p>text</p>
+      </Tab>
 
-    <Tab name="Caracteristicas">
-      <p>text</p>
-    </Tab>
-  </Tabs>
+      <Tab name="Caracteristicas">
+        <p>text</p>
+      </Tab>
+    </Tabs>
+  </div>
 </template>
 
 <script setup>
@@ -30,7 +44,8 @@ import Tabs from './components/Tabs.vue'
 import Tab from './components/Tab.vue'
 
 const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const selected = ref('level_1')
+const selected_level = ref('level_1')
+const selected_type = ref('fight')
 
 function level_key(level) {
   return `level_${level}`
